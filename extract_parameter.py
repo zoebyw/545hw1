eab6c8a32c53d11c0ae6e9c0cda4f602434cdc50
Zoe Williams <zbyw@uw.edu>
2025-01-19 19:45:51 -0800

    added code from problem statement

--- extract_parameter.py
+++ extract_parameter.py
@@ -1,3 +1,12 @@
+
+unit_operations_data = {
+    "distillation_column": {"temperature": [150, 160, 170], "pressure": [2, 2.5, 3], "flow_rate": [100, 110, 120]},
+    "reactor": {"temperature": [250, 260, 270], "pressure": [5, 5.5, 6], "residence_time": [10, 12, 14]},
+    "heat_exchanger": {"temperature_in": [80, 90, 100], "temperature_out": [50, 60, 70], "flow_rate": [200, 210, 220]}
+}
+
+
+
 def extract_parameter(unit_operations_data, unit_name, parameter_name, index):
     try:
         value = unit_operations_data[unit_name][parameter_name][index] #accessing the values directly using keys
