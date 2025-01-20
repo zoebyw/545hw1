32a616838c6eb9affe996d795b995ee7cff4f38b
Zoe Williams <zbyw@uw.edu>
2025-01-19 19:07:28 -0800

    This is my code for question 2 of hw 1. I included my notes and links to sources that helped me

---
+++ calculate_solution_weights.py
@@ -0,0 +1,17 @@
+def calculate_solution_weights(molecular_weights,solutions_needed):
+    result = [] #creating blank list for results
+    for solution in solutions_needed: # looking among the entries in the solutions_needed list
+        compound, concentration = solution.split('-') # splitting the solutions_needed list into sublists with the compound name and desired concentration
+        concentration = float(concentration.replace('M','')) # taking the concentration from solutions_needed and turning it into a float number that can be multiplied by molecular_weight later on
+                                                            # this is also getting rid of the unit 'M'
+        if compound in molecular_weights: 
+            molecular_weight = molecular_weights[compound] #accessing value from dictionary using key
+            weight = molecular_weight * concentration #calculating weight of compound in 1 L solution
+            result.append(f"{compound}-{concentration}M-{weight:.2f}g") # formatting the return function, including units and rounding the weight to two decimal places
+        else:
+            result.append("unknown") #will return "unknown" if chemical compound not in molecular_weights
+    return result
+print(calculate_solution_weights(molecular_weights,solutions_needed))
+
+# help with split and float: https://www.geeksforgeeks.org/python-string-split/#
+# help with append: https://www.w3schools.com/python/ref_list_append.asp
